723e07d39ad9d284656fa6cad4789bfa6237990c
Yogi Pratama <yogi.prtma07@gmail.com>
2021-08-03 09:20:05 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0', // ternary operator untuk nilai environment yg ditetapkan
     routes: { // CORS (Cross-origin resource sharing) bawaan HAPI > untuk mengizinkan akses origin
       cors: {
         origin: ['*'],
